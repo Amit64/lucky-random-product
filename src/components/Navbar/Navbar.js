d0a28f7e86e3a9591ec163013dc8e6b70997d90c
Amit DebNath <amitdebnath@Amits-MacBook-Pro.local>
2022-03-30 00:02:56 +0600

    set up navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css'
+
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+            <h1>Smart Watch Shop</h1>
+        </nav>
+    );
+};
+
+export default Navbar;
